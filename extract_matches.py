886c8151d9cdf830e512a51231fefe1e15ac8ad4
Keild <osipov-nik2002@mail.ru>
2026-05-03 22:43:46 +0300

    test

--- extract_matches.py
+++ extract_matches.py
@@ -5,7 +5,7 @@ from datetime import datetime
 API_URL = "https://api-football-v1.p.rapidapi.com/v3/fixtures&quot";
 HEADERS = {
     "X-RapidAPI-Key": "84e4935deemshddcbb45293698d2p194d3djsn31e7905367b8",
-    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com
+    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 }
 DB_PARAMS={
 "dbname":"football_db",
@@ -25,31 +25,31 @@ def get_matches(date_str):
 	return response.json()["response"]
 
 def load_to_db(matches):
-conn=psycopg2.connect(**DB_PARAMS)
-cur=conn.cursor()
+	conn=psycopg2.connect(**DB_PARAMS)
+	cur=conn.cursor()
 
-for m in matches:
-	cur.execute("""
-		Insert INTO matches (match_id, league_name, match_date, home_team, away_team,
-		status, home_goals, away_goals)
-		VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
-		ON CONFLICT (match_id) DO UPDATE SET
-			status= EXCLUDED.status,
-			home_goals=EXCLUDED.home_goals,
-                away_goals = EXCLUDED.away_goals;
-        """, (m['fixture']['id'],
-		m['league']['name'],
-		m['fixture']['date'],
-         	 m['teams']['home']['name'],
-          	 m['teams']['away']['name'],
-          	 m['fixture']['status']['short'],
-           	 m['goals']['home'],
-           	 m['goals']['away']
-        ))
-conn.commit()
-cur.close()
-conn.close()
-print(f"Загружено {len(matches)} матчей.")
+	for m in matches:
+		cur.execute("""
+			Insert INTO matches (match_id, league_name, match_date, home_team, away_team,
+			status, home_goals, away_goals)
+			VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+			ON CONFLICT (match_id) DO UPDATE SET
+				status= EXCLUDED.status,
+				home_goals=EXCLUDED.home_goals,
+					away_goals = EXCLUDED.away_goals;
+			""", (m['fixture']['id'],
+			m['league']['name'],
+			m['fixture']['date'],
+				m['teams']['home']['name'],
+				m['teams']['away']['name'],
+				m['fixture']['status']['short'],
+				m['goals']['home'],
+				m['goals']['away']
+			))
+	conn.commit()
+	cur.close()
+	conn.close()
+	print(f"Загружено {len(matches)} матчей.")
 
 if __name__=="__main__":
 	today=datetime.now().strftime('%Y-%m-%d')
@@ -58,3 +58,8 @@ if __name__=="__main__":
 		load_to_db(data)
 	else:
 		print("Сегодня матчей нет.")
+
+
+
+
+
